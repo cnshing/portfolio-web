e043a2adc147cf76f623485f7470e0e36e082503
Shing Chan <chan.shing@protonmail.com>
2025-10-16 17:02:50 -0700

    feat(app): Add sample button to app page for rendering

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { ZardButtonComponent } from '@shared/components/button/button.component';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html'
+  templateUrl: './app.html',
+  imports: [ZardButtonComponent]
 })
 export class App {}
