2347c9bda5823fba5ea6f5b20a2a376a87f0a9b6
Shing Chan <chan.shing@protonmail.com>
2025-10-17 18:40:35 -0700

    feat(app): Add test `z-divider` and `z-avatar` to main render page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ZardButtonComponent } from '@shared/components/button/button.component';
+import { ZardAvatarComponent } from '@shared/components/avatar/avatar.component';
+import { ZardDividerComponent } from '@shared/components/divider/divider.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [ZardButtonComponent]
+  imports: [ZardButtonComponent, ZardAvatarComponent, ZardDividerComponent]
 })
 export class App {}
