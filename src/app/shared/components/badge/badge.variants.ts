35fc533fcb71c83454d1d66a90a0259f40b1328c
Shing Chan <chan.shing@protonmail.com>
2025-10-19 17:17:38 -0700

    refactor(baddge): Make shape default `square` and restyle badge

--- src/app/shared/components/badge/badge.variants.ts
+++ src/app/shared/components/badge/badge.variants.ts
@@ -1,23 +1,23 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
 export const badgeVariants = cva(
-  'inline-flex items-center border text-xs px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center border text- px-xs py-3xs font-secondary transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       zType: {
-        default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        destructive: 'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        outline: 'text-foreground',
+        default: 'border-transparent bg-color-button text-on-bg-button hover:bg-color-button/85',
+        secondary: 'border-transparent bg-color-surface2 text-color-secondary hover:bg-color-surface2/70',
+        destructive: 'border-transparent bg-destructive text-color-default hover:bg-destructive/85',
+        outline: 'border-color-subtle border text-on-bg-secondary'
       },
       zShape: {
         default: 'rounded-full',
-        square: 'rounded-none',
+        square: 'rounded-xs',
       },
     },
     defaultVariants: {
       zType: 'default',
-      zShape: 'default',
+      zShape: 'square',
     },
   },
 );
