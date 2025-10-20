d6761a831b612919a266844f7384ba94cd968424
Shing Chan <chan.shing@protonmail.com>
2025-10-20 15:19:32 -0700

    refactor(skeleton): Change bg color to `surface2` for `skeleton.variants.ts`

--- src/app/shared/components/skeleton/skeleton.variants.ts
+++ src/app/shared/components/skeleton/skeleton.variants.ts
@@ -1,4 +1,4 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
-export const skeletonVariants = cva('bg-accent animate-pulse rounded-md');
+export const skeletonVariants = cva('bg-color-surface2 animate-pulse rounded-md');
 export type ZardSkeletonVariants = VariantProps<typeof skeletonVariants>;
