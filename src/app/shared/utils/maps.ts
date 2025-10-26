e21010a13af814774cd9ed208c9a3a532c8affd3
Shing Chan <chan.shing@protonmail.com>
2025-10-26 03:12:01 -0700

    refactor(maps): Add key map support for `ObjectMap` in `maps.ts`

--- src/app/shared/utils/maps.ts
+++ src/app/shared/utils/maps.ts
@@ -1,24 +1,25 @@
-
 /**
  * Applies a function to every key, value in the object
  * Modified from https://stackoverflow.com/a/74553065.
- * @export
+ @export
  * @template {PropertyKey} K Any key of `obj`.
  * @template V Any value of `obj`.
- * @template R Any arbitrary result of `callbackfn(k, v)`
- * @param {{ [P in K]: V }} obj Object to apply `callbackfn(k, v)`
- * @param {(key: K, value: V, index?: number) => R} callbackfn Any function that takes in the obj's key and value and returns any result.
- * @returns {{ [P in K]: R }} A new object representing { k: callbackfn(k, v) for k, v in obj}
+ * @template RV Any arbitrary result of `keyfn(k, v)`
+ * @template {PropertyKey} [RK=K] Any arbitrary result of `keyfn(k, v)`
+ * @param {{ [P in K]: V }} obj
+ * @param {(key: K, value: V, index?: number) => RV} [valuefn=(_, value) => value as unknown as RV] Any function that takes in the obj's key, value, and optional index number to return a result.
+ * @param {(key: K, value: V, index?: number) => RK} [keyfn=(key) => key as unknown as RK] Any function that takes in the obj's key, value, and optional index number to return a result.
+ * @returns {{ [P in RK]: RV }} A new object representing { keyfn(k, v): valuefn(k, v) for k, v in obj}
  */
-export function ObjectMap<K extends PropertyKey, V, R>(
+
+export function ObjectMap<K extends PropertyKey, V, RV, RK extends PropertyKey = K>(
   obj: { [P in K]: V },
-  callbackfn: (key: K, value: V, index?: number) => R,
-): { [P in K]: R } {
+  valuefn: (key: K, value: V, index?: number) => RV = (_, value) => value as unknown as RV,   keyfn: (key: K, value: V, index?: number) => RK = (key) => key as unknown as RK,
+): { [P in RK]: RV } {
   return Object.fromEntries(
     Object.entries(obj)
-      .map(([key, value], index) => [key, callbackfn(key as K, value as V, index)]),
+      .map(([key, value], index) => [keyfn(key as K, value as V, index), valuefn(key as K, value as V, index)]),
     // assert --------------------------------> ^^^^^
-  ) as { [P in K]: R }
+  ) as { [P in RK]: RV }
   //^^^^^^^^^^^^^^^^^^ <-- assert
 }
-
