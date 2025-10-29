bf7d31de143298e0715bc4c1bfc695811be5f223
Shing Chan <chan.shing@protonmail.com>
2025-10-29 04:35:31 -0700

    revert(map): Remove index support for `ObjectMap` in `maps.ts`
    Since ES2015-2016 map iteration order is not by insertion order. Explicit control over index by enumerating values seems to be the way to go

--- src/app/shared/utils/maps.ts
+++ src/app/shared/utils/maps.ts
@@ -1,3 +1,4 @@
+
 /**
  * Applies a function to every key, value in the object
  * Modified from https://stackoverflow.com/a/74553065.
@@ -6,20 +7,20 @@
  * @template V Any value of `obj`.
  * @template RV Any arbitrary result of `keyfn(k, v)`
  * @template {PropertyKey} [RK=K] Any arbitrary result of `keyfn(k, v)`
- * @param {{ [P in K]: V }} obj
- * @param {(key: K, value: V, index?: number) => RV} [valuefn=(_, value) => value as unknown as RV] Any function that takes in the obj's key, value, and optional index number to return a result.
- * @param {(key: K, value: V, index?: number) => RK} [keyfn=(key) => key as unknown as RK] Any function that takes in the obj's key, value, and optional index number to return a result.
+ * @param {{ [P in K]: V }} obj The object to map.
+ * @param {(key: K, value: V) => RV} valuefn Any function that takes in the obj's key, value to return a result.
+ * @param {(key: K, value: V) => RK} keyfn Any function that takes in the obj's key, value to return a result.
  * @returns {{ [P in RK]: RV }} A new object representing { keyfn(k, v): valuefn(k, v) for k, v in obj}
  */
-
 export function ObjectMap<K extends PropertyKey, V, RV, RK extends PropertyKey = K>(
   obj: { [P in K]: V },
-  valuefn: (key: K, value: V, index?: number) => RV = (_, value) => value as unknown as RV,   keyfn: (key: K, value: V, index?: number) => RK = (key) => key as unknown as RK,
+  valuefn: (value: V, key: K) => RV = (value) => value as unknown as RV,   keyfn: (key: K, value: V) => RK = (key) => key as unknown as RK,
 ): { [P in RK]: RV } {
   return Object.fromEntries(
     Object.entries(obj)
-      .map(([key, value], index) => [keyfn(key as K, value as V, index), valuefn(key as K, value as V, index)]),
+      .map(([key, value]) => [keyfn(key as K, value as V), valuefn(value as V, key as K)]),
     // assert --------------------------------> ^^^^^
   ) as { [P in RK]: RV }
   //^^^^^^^^^^^^^^^^^^ <-- assert
 }
+
