8732a5b6c88189243e4bdb9838d3186ea40e5234
Shing Chan <chan.shing@protonmail.com>
2025-10-28 23:05:12 -0700

    refactor(icon): Fix dynamic icon loading support
    Moved icon loader to icon.ts, refactored icon property retrival to use internal ng-icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,26 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideNgIconLoader, withCaching } from '@ng-icons/core';
-import { HttpClient } from '@angular/common/http';
-import { inject } from '@angular/core';
-
-/**
- * Dynamically retrieves an icon.
- * Modified from https://github.com/ng-icons/ng-icons?tab=readme-ov-file#dynamically-loading-icons
- * @param {string} name Filename of the icon.
- * @returns {Observable(string)} SVG string representation of `name`.
- */
-const iconLoader = (name: string) => {
-  const http = inject(HttpClient);
-  return http.get(`/assets/icons/${name}.svg`, { responseType: 'text' });
-}
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideNgIconLoader(iconLoader, withCaching())
+    provideHttpClient(withFetch())
   ]
 };
