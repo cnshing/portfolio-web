85637c9e06678de83a230ad28382f48cc07fe1ce
Shing Chan <chan.shing@protonmail.com>
2025-10-31 06:48:49 -0700

    test(layout): Revert old attempted DRY code in `footer.spec.ts`

--- src/app/shared/components/layout/footer.spec.ts
+++ src/app/shared/components/layout/footer.spec.ts
@@ -2,7 +2,6 @@ import {inputBinding, provideZonelessChangeDetection} from '@angular/core'
 import {render, screen,} from '@testing-library/angular'
 import { FooterComponent } from './footer.component'
 import { TestBed } from '@angular/core/testing'
-import { ComponentType } from '@angular/cdk/overlay';
 describe("Primitive Footer Tests", () => {
 
   beforeEach(async () => {
@@ -11,18 +10,14 @@ describe("Primitive Footer Tests", () => {
     });
   })
   it("When given a zHeight, the styled height should be identical in rem", async () => {
-    testHeight(FooterComponent, 9)
-  })
-})
-
-
-export const testHeight = async<T>(component: ComponentType<T>, zHeight:number) => {
-  async () => {
-    await render(component, {
+    const zHeight = 7
+    await render(FooterComponent, {
       bindings: [inputBinding('zHeight', () => zHeight)]
     })
     const footer = screen.getByRole('contentinfo')
     const actualHeight = parseInt(footer.style.height)
     expect(actualHeight).toBe(zHeight)
-  }
-}
+  })
+})
+
+
