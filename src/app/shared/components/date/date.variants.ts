193f716cce13f988bbfe79ef7c786ce8dbeb6e6b
Shing Chan <chan.shing@protonmail.com>
2025-11-05 02:09:09 -0800

    revert(date): Remove `zType` variants for date component
    Primitive text should not need special zType inputs

--- src/app/shared/components/date/date.variants.ts
+++ src/app/shared/components/date/date.variants.ts
@@ -1,21 +1,15 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
-const dateVariants = cva('', {
+const dateVariants = cva('text-inherit', {
   variants: {
     zSize: {
       sm: 'text-sm',
       default: 'text-sm',
       lg: 'text-lg',
-    },
-    zType: {
-      default: 'text-color-default',
-      outline: 'text-color-default',
-      ghost: 'text-color-secondary',
-    },
+    }
   },
   defaultVariants: {
-    zSize: 'default',
-    zType: 'outline',
+    zSize: 'default'
   },
 });
 
