2b080193b0cb6743b27beb46f28ac205f9a4e1ad
Shing Chan <chan.shing@protonmail.com>
2025-11-05 05:43:58 -0800

    feat(playground): Add UI Kitchen sink playground page

---
+++ src/app/features/playground/playground-card.ts
@@ -0,0 +1,24 @@
+import { Component, input } from '@angular/core';
+
+/**
+ * Container for any primiitive component.  Primitive components should not be used to create any playground components as failures in any primitive component can prevent a playground component from rendering. Playground components should be as simple as possible to reduce complexity and prone to error.
+ *
+ * @export
+ * @class PlaygroundCardComponent
+ * @typedef {PlaygroundCardComponent}
+ */
+@Component({
+  selector: 'playground-card',
+  standalone: true,
+  template: `
+    <div class="bg-color-surface1 border border-color-subtle rounded-md p-lg min-w-fit flex flex-col gap-md">
+      @if (title()) {
+        <h4>{{ title() }}</h4>
+      }
+      <ng-content></ng-content>
+    </div>
+  `,
+})
+export class PlaygroundCardComponent {
+  readonly title = input<string>()
+}
