c51ee2caf9a2329fb5ed99e0066d32399e63ba20
Shing Chan <chan.shing@protonmail.com>
2025-11-14 03:34:09 -0800

    refactor(landing): Add blank landing page and change routes

---
+++ src/app/features/landing/landing-page.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'landing-page',
+  standalone: true,
+  imports: [],
+  template: `
+  <div>
+    Hello World
+</div>
+  `
+})
+export class LandingPageComponent {
+
+}
