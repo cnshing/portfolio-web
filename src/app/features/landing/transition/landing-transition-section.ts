58f280169b77a38f123c9319db45d3303944b4e5
Shing Chan <chan.shing@protonmail.com>
2025-11-16 03:33:03 -0800

    docs(landing): Add JSDoc to `landing-transition-section.ts`

--- src/app/features/landing/transition/landing-transition-section.ts
+++ src/app/features/landing/transition/landing-transition-section.ts
@@ -2,6 +2,13 @@ import { Component, input } from "@angular/core";
 import { NgClass } from "@angular/common";
 
 
+/**
+ * A small section with glowing neon 2D "race tracks".
+ *
+ * @export
+ * @class LandingTransitionComponent
+ * @typedef {LandingTransitionComponent}
+ */
 @Component({
   selector: 'landing-transition',
   standalone: true,
@@ -10,5 +17,11 @@ import { NgClass } from "@angular/common";
   styleUrl: './landing-transition-section.sass'
 })
 export default class LandingTransitionComponent {
+  /**
+   * From which direction should the line start?
+   *
+   * @readonly
+   * @type {*}
+   */
   readonly direction = input.required<"left"|"right">()
 }
