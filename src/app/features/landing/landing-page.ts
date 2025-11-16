5cdb3d0fddc9a5fcf9893eb08881afb5ad33a2b8
Shing Chan <chan.shing@protonmail.com>
2025-11-16 03:33:36 -0800

    feat(landing): Add transition section to `landing-page.ts`

--- src/app/features/landing/landing-page.ts
+++ src/app/features/landing/landing-page.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { LandingHeroComponent } from "@features/landing/landing-hero-section";
+import LandingTransitionComponent from "@features/landing/transition/landing-transition-section";
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [LandingHeroComponent],
+  imports: [LandingHeroComponent, LandingTransitionComponent],
   template: `
   <landing-hero />
+  <landing-transition direction="left">
   `
 })
 export class LandingPageComponent {
