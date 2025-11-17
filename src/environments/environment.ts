beefd7b9eabb6f19ef6a139e86df16ec781394d4
Shing Chan <chan.shing@protonmail.com>
2025-11-17 01:48:12 -0800

    Add basic profile information in

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  name: "FirstName LastName",
+  location: "City, State",
+  githubUsername: "github",
+  linkedinUsername: "linkedin",
+  phoneNumber: "+1 415 555 2671"
+};
