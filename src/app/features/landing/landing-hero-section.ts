bfd925b639269d9b5a19033f0f2dd6957e56c029
Shing Chan <chan.shing@protonmail.com>
2025-11-20 03:23:58 -0800

    refactor(landing): Adjust responisveness for hero and landing page and hero

--- src/app/features/landing/landing-hero-section.ts
+++ src/app/features/landing/landing-hero-section.ts
@@ -8,23 +8,25 @@ import { ZardIconComponent } from "@shared/components/icon/icon.component";
   standalone: true,
   imports: [ZardButtonComponent, ZardIconComponent],
   template: `
-  <section class="relative bg-color-page flex flex-wrap justify-content">
-    <div class="flex flex-col gap-lg py-2xl pl-3xl max-w-[50vw] z-1">
+  <section class="flex">
+    <div class="flex flex-col gap-lg max-w-line-length z-1">
       <div>
       <h1 class="text-hero-accent">Zooming</h1>
       <h1 class="inline-block">Full Stack Developer</h1>
       </div>
-      <p class="text-xl"><span class="text-color-accent">{{ name }}</span> here — a software engineer experienced in frontend and middleware development for ROS (Robot Operating System).</p>
+      <p><span class="text-color-accent">{{ name }}</span> here — a software engineer experienced in frontend and middleware development for ROS (Robot Operating System).</p>
       <button z-button class="w-min">
         <i z-icon zSize="lg" zType="resume"></i>
         View Resume
       </button>
     </div>
-    <video autoplay loop class="absolute right-0 bottom-0 w-[75vw] h-full py-2xl">
+    <video autoplay loop class="max-w-full">
       <source src="assets/videos/motorcycle.mp4" type="video/mp4">
     </video>
   </section>
-  `
+  `,
+
+
 })
 export default class LandingHeroComponent {
   protected readonly name = environment.name
--- src/app/features/landing/landing-page.ts
+++ src/app/features/landing/landing-page.ts
@@ -1,14 +1,30 @@
 import { Component } from "@angular/core";
-import { LandingHeroComponent } from "@features/landing/landing-hero-section";
+import LandingHeroComponent from "@features/landing/landing-hero-section";
 import LandingTransitionComponent from "@features/landing/transition/landing-transition-section";
+import LandingAboutMeComponent  from "@features/landing/about-me/landing-about-me-section";
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [LandingHeroComponent, LandingTransitionComponent],
+  imports: [LandingHeroComponent, LandingTransitionComponent, LandingAboutMeComponent],
   template: `
   <landing-hero />
-  <landing-transition direction="left">
+  <landing-transition class=""/>
+  <landing-about-me/>
+  <landing-transition class="scale-y-[-1] scale-x-[-1]"/>
+  `,
+  host: {
+    'class': 'flex flex-col w-full bg-color-page page',
+  },
+  styles: `
+
+  ::ng-deep .page > :not(landing-transition) > *
+    max-width: var(--spacing-max-width)
+    padding: var(--spacing-2xl) var(--spacing-lg)
+    margin-left: auto
+    margin-right: auto
+    gap: var(--spacing-xl)
+    width: 100%
   `
 })
 export class LandingPageComponent {
