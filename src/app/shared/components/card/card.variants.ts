1d80546aa0e9f5dcdb520eb9674a3cc30e1049b2
Shing Chan <chan.shing@protonmail.com>
2025-11-25 05:22:42 -0800

    refactor(card): Increase spacing from `xs` to `lg` in `card.variants.ts`
    Gap space made all the text too jumbled together

--- src/app/shared/components/card/card.variants.ts
+++ src/app/shared/components/card/card.variants.ts
@@ -1,11 +1,11 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
-export const cardVariants = cva('rounded-md border border-color-default bg-color-surface1 text-color-primary shadow-sm min-w-fit w-full p-lg flex flex-col gap-y-xs', {
+export const cardVariants = cva('rounded-md border border-color-default bg-color-surface1 text-color-primary shadow-sm min-w-fit w-full p-lg flex flex-col gap-y-lg', {
   variants: {},
 });
 export type ZardCardVariants = VariantProps<typeof cardVariants>;
 
-export const cardHeaderVariants = cva('flex flex-wrap gap-x-sm gap-y-xs items-center', {
+export const cardHeaderVariants = cva('flex flex-wrap gap-x-sm gap-y-lg items-center', {
   variants: {},
 });
 export type ZardCardHeaderVariants = VariantProps<typeof cardHeaderVariants>;
