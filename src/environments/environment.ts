4c41db245c33c659314ab596a77ad071b3749ad1
Shing Chan <chan.shing@protonmail.com>
2025-11-25 04:57:59 -0800

    fix(landing): Allow raw string import of career content

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,14 @@
+import AgapeOne from '@content/career/agape-1.md' with { loader: 'text' }
+import Intelcon from '@content/career/intelcon.md' with { loader: 'text' }
+import UCSC from '@content/career/ucsc.md' with { loader: 'text' } // From https://github.com/angular/angular-cli/issues/26575
+
+
 export const environment = {
   name: "firstName lastName",
   location: "City, State",
   email: "my@email.com",
   githubUsername: "githubName",
   linkedinUsername: "linkedinHandle",
-  phoneNumber: "+14155552671"
+  phoneNumber: "+14155552671",
+  landingCareerContentMDs: [AgapeOne, Intelcon, UCSC]
 } as const;
