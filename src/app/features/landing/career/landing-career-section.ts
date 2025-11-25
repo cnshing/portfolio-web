d3de2c3c745f084dd3277c4e62edd874963cab33
Shing Chan <chan.shing@protonmail.com>
2025-11-25 04:55:32 -0800

    feat(landing): Add landing career section

---
+++ src/app/features/landing/career/landing-career-section.ts
@@ -0,0 +1,62 @@
+import { Component, computed, inject, Signal } from '@angular/core';
+
+import LandingCareerPositionComponent from '@features/landing/career/experience/landing-career-experience';
+
+import { environment } from '@environments/environment';
+
+import { LandingCareerExperienceInput } from './experience/landing-career-experience.types';
+import { LandingCareerMarkdown } from './services/landing-career-markdown.service';
+
+/**
+ * Section containing user's career experience.
+ *
+ * @export
+ * @class LandingCareerComponent
+ * @typedef {LandingCareerComponent}
+ */
+@Component({
+  selector: 'landing-career-timeline',
+  standalone: true,
+  providers: [LandingCareerMarkdown],
+  imports: [LandingCareerPositionComponent],
+  template: `
+    <section>
+      <h1 class="text-center -translate-y-1/2">Career Timeline</h1>
+      <div class="grid auto-grid-[100%] gap-xl">
+        @for (position of positions(); track position.company) {
+        <landing-career-position
+          [company]="position.company"
+          [position]="position.position"
+          [summary]="position.summary"
+          [skills]="position.skills ?? []"
+          [highlights]="position.highlights"
+          [from]="position.from"
+          [to]="position.to"
+          [companyLogoImg]="position.companyLogoImg"
+        />
+        }
+      </div>
+    </section>
+  `,
+})
+export default class LandingCareerComponent {
+  /**
+   * Helper service required to load career experience content
+   *
+   * @protected
+   * @readonly
+   * @type {*}
+   */
+  protected readonly markdownParser = inject(LandingCareerMarkdown);
+
+  /**
+   * Parsed content containing the neccesary inputs to render `LandingCareerExperience` components.
+   *
+   * @protected
+   * @readonly
+   * @type {Signal<LandingCareerExperienceInput[]>}
+   */
+  protected readonly positions: Signal<LandingCareerExperienceInput[]> = computed(() =>
+    environment.landingCareerContentMDs.map((md) => this.markdownParser.parseMarkdown(md))
+  );
+}
