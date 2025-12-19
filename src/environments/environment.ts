2d60a7f1c368d80716a35a9dcdd314d287356591
Shing Chan <chan.shing@protonmail.com>
2025-12-18 19:28:37 -0800

    feat(skills)!: Add enviornment work in progress content in `enviornment.ts`

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 import AgapeOne from '@content/career/agape-1.md' with { loader: 'text' }
 import Intelcon from '@content/career/intelcon.md' with { loader: 'text' }
 import UCSC from '@content/career/ucsc.md' with { loader: 'text' } // From https://github.com/angular/angular-cli/issues/26575
+import { LandingSkillCardInput } from '@features/landing/skills/landing-skills-card';
 
 
 export const environment = {
@@ -10,5 +11,21 @@ export const environment = {
   githubUsername: "githubName",
   linkedinUsername: "linkedinHandle",
   phoneNumber: "+14155552671",
-  landingCareerContentMDs: [AgapeOne, Intelcon, UCSC]
+  landingCareerContentMDs: [AgapeOne, Intelcon, UCSC],
+  landingSkillsContent: [
+    {
+      name: "Docker",
+      skillImg: "assets/icons/docker-mark-blue.svg",
+      description: "Test description"
+    },
+    {
+      name: "Next.JS",
+      skillImg: "assets/icons/nextjs-icon-light-background.svg"
+    },
+    {
+      name: "Figma",
+      skillImg: "assets/icons/Figma Icon (Full-color).svg",
+      description: "Designed this site with Figma."
+    }
+  ] as LandingSkillCardInput[]
 } as const;
