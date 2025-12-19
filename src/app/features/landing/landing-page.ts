03afc53d5ef70206fba063ab179c8bc73ca87416
Shing Chan <chan.shing@protonmail.com>
2025-12-18 19:32:15 -0800

    feat(landig): Add landing-skills to `landing-page.ts`

--- src/app/features/landing/landing-page.ts
+++ src/app/features/landing/landing-page.ts
@@ -3,17 +3,21 @@ import LandingHeroComponent from "@features/landing/landing-hero-section";
 import LandingTransitionComponent from "@features/landing/transition/landing-transition-section";
 import LandingAboutMeComponent  from "@features/landing/about-me/landing-about-me-section";
 import LandingCareerComponent from "@features/landing/career/landing-career-section";
+import LandingSkillsComponent from "@features/landing/skills/landing-skills-section";
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [LandingHeroComponent, LandingTransitionComponent, LandingAboutMeComponent, LandingCareerComponent],
+  imports: [LandingHeroComponent, LandingTransitionComponent, LandingAboutMeComponent, LandingCareerComponent,
+  LandingSkillsComponent
+  ],
   template: `
   <landing-hero />
   <landing-transition class=""/>
   <landing-about-me/>
   <landing-transition class="scale-y-[-1] scale-x-[-1]"/>
   <landing-career-timeline />
+  <landing-skills />
   `,
   host: {
     'class': 'flex flex-col w-full bg-color-page page',
