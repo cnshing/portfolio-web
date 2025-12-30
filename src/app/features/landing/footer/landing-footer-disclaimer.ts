d5a393815abdaa9d513757d061609cc53e61d1a8
Shing Chan <chan.shing@protonmail.com>
2025-12-30 06:58:28 -0800

    feat(footer): Add complete landing footer section

---
+++ src/app/features/landing/footer/landing-footer-disclaimer.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+
+/**
+ * Small copyright disclaimer.
+ *
+ * @export
+ * @class LandingFooterDisclaimerComponent
+ * @typedef {LandingFooterDisclaimerComponent}
+ */
+@Component({
+  selector: 'landing-footer-disclaimer',
+  standalone: true,
+  providers: [],
+  imports: [],
+  template: `
+    <section class="text-color-tertiary flex flex-wrap justify-between gap-y-2xs">
+      <small>Developed by {{ copyrightOwner }} via Angular.</small>
+      <small>© {{ copyrightYears }} {{ copyrightOwner }}. All Rights Reserved.</small>
+    </section>
+  `,
+})
+export class LandingFooterDisclaimerComponent {
+  /**
+   * Copyright owner of this site.
+   *
+   * @protected
+   * @readonly
+   * @type {*}
+   */
+  protected readonly copyrightOwner = environment.name;
+
+  /**
+   * Copyright year range.
+   *
+   * @protected
+   * @readonly
+   * @type {string}
+   */
+  protected readonly copyrightYears = `2025-${new Date().getFullYear()}`;
+}
