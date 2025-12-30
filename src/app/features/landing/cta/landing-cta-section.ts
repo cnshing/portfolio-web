19f5135ac4181c1e6fe5947a864a48018f518370
Shing Chan <chan.shing@protonmail.com>
2025-12-30 06:49:14 -0800

    fix(cta): Add CTA landing section padding and remove un-needed styling in `landing-cta-section.ts`

--- src/app/features/landing/cta/landing-cta-section.ts
+++ src/app/features/landing/cta/landing-cta-section.ts
@@ -16,8 +16,8 @@ import { ZardIconComponent } from '@shared/components/icon/icon.component';
   providers: [],
   imports: [ZardButtonComponent, ZardIconComponent],
   template: `
-    <section class="flex flex-col gap-2xl">
-      <div class="m-auto text-center">
+    <section>
+      <div class="m-auto text-center py-3xl">
          <h1 class="mb-lg" >Any questions?  I’d be happy to answer any inquiries.</h1>
          <a href="mailto:{{ email }}">
           <button z-button class="w-min">
