d95e5b4a92e9467e376076a8433a704077e0b25c
Shing Chan <chan.shing@protonmail.com>
2026-01-01 02:48:09 -0800

    feat(landing): Integrate `CTA`, `Footer`, and new `Transition` components into `landing-page.ts`

--- src/app/features/landing/landing-page.ts
+++ src/app/features/landing/landing-page.ts
@@ -1,32 +1,36 @@
 import { Component } from "@angular/core";
 import LandingHeroComponent from "@features/landing/landing-hero-section";
-import LandingTransitionComponent from "@features/landing/transition/landing-transition-section";
+import { LandingTransitionRacetrackComponent, LandingTransitionHelmetComponent } from "@features/landing/transition/landing-transition-section";
 import LandingAboutMeComponent  from "@features/landing/about-me/landing-about-me-section";
 import LandingCareerComponent from "@features/landing/career/landing-career-section";
 import LandingSkillsComponent from "@features/landing/skills/landing-skills-section";
 import LandingProjectsComponent from "@features/landing/projects/landing-projects-section";
+import LandingFooterComponent from "@features/landing/footer/landing-footer-section";
+import LandingCTAComponent from "@features/landing/cta/landing-cta-section";
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [LandingHeroComponent, LandingTransitionComponent, LandingAboutMeComponent, LandingCareerComponent,
-  LandingSkillsComponent, LandingProjectsComponent
-  ],
+  imports: [LandingHeroComponent, LandingTransitionRacetrackComponent, LandingAboutMeComponent, LandingCareerComponent,
+    LandingSkillsComponent, LandingProjectsComponent, LandingFooterComponent, LandingCTAComponent, LandingTransitionHelmetComponent],
   template: `
   <landing-hero />
-  <landing-transition class=""/>
+  <landing-transition-racetrack />
   <landing-about-me/>
-  <landing-transition class="scale-y-[-1] scale-x-[-1]"/>
+  <landing-transition-racetrack class="scale-y-[-1] scale-x-[-1]"/>
   <landing-career-timeline />
   <landing-skills />
-  <landing-projects>
+  <landing-transition-helmet />
+  <landing-projects/>
+  <landing-cta/>
+  <landing-footer/>
   `,
   host: {
     'class': 'flex flex-col w-full bg-color-page page',
   },
   styles: `
 
-  ::ng-deep .page > :not(landing-transition) > *
+  ::ng-deep .page > :not(landing-transition-helmet, landing-transition-racetrack) > *
     max-width: var(--spacing-max-width)
     padding: var(--spacing-2xl) var(--spacing-lg)
     margin-left: auto
