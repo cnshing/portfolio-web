431e95cb312cbdb54be6dcadc794a1ce264eeb01
Shing Chan <chan.shing@protonmail.com>
2026-01-11 23:16:39 -0800

    chore(landing): Prettify `landing-linux-card.ts`

--- src/app/features/landing/projects/linux/landing-linux-card.ts
+++ src/app/features/landing/projects/linux/landing-linux-card.ts
@@ -17,8 +17,10 @@ import { LandingProjectsCardComponent } from '@features/landing/projects/landing
     <landing-projects-card
       class="bg-[#F8BF11] p-md flex flex-col justify-center border border-color-[#202020]"
     >
-      <h2 class="inline-flex flex-wrap text-[#202020] justify-center gap-y-xs items-center text-right max-[31.875rem]:text-center">
-        Custom<br class="md:hidden"/>OS<img class="size-2xl" src="assets/icons/linux.svg" />
+      <h2
+        class="inline-flex flex-wrap text-[#202020] justify-center gap-y-xs items-center text-right max-[31.875rem]:text-center"
+      >
+        Custom<br class="md:hidden" />OS<img class="size-2xl" src="assets/icons/linux.svg" />
       </h2>
     </landing-projects-card>
   `,
