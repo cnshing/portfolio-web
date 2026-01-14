6900807b4926ae4d0ceaedebeb2a22ab695de5c0
Shing Chan <chan.shing@protonmail.com>
2026-01-14 00:49:57 -0800

    refactor(landing): Add button accessibility to me-avatar interaction

--- src/app/features/landing/about-me/landing-about-me-profile.ts
+++ src/app/features/landing/about-me/landing-about-me-profile.ts
@@ -11,8 +11,16 @@ import { environment } from '@environments/environment';
   template: `
     <div class="flex flex-col gap-xl">
       <div class="relative">
-        <p class="text-center py-sm md:absolute text-xl md:text-left md:top-1/2 md:-translate-y-1/2 md:left-[calc(62.5%+var(--spacing-xl))] font-[Nanum_Pen_Script]"><span class="hidden md:contents">↩&nbsp;&nbsp;</span>Click "me" for a surprise!</p>
-        <me-avatar (click)="this.onRandomAvatar($event)" [posture]="this.posture()"></me-avatar>
+        <p
+          class="text-center py-sm md:absolute text-xl md:text-left md:top-1/2 md:-translate-y-1/2 md:left-[calc(62.5%+var(--spacing-xl))] font-[Nanum_Pen_Script]"
+        >
+          <span class="hidden md:contents">↩&nbsp;&nbsp;</span>Click "me" for a surprise!
+        </p>
+        <div class="flex justify-center">
+          <button class="w-min rounded-full" (click)="this.onRandomAvatar($event)">
+            <me-avatar [posture]="this.posture()"></me-avatar>
+          </button>
+        </div>
       </div>
       <div class="flex flex-wrap justify-center-safe gap-lg">
         <a href="tel:{{ phone }}">
