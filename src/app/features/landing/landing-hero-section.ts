1377195efc3fc73982051f62417cac8dc3ab439a
Shing Chan <chan.shing@protonmail.com>
2026-01-14 01:08:13 -0800

    feat(landing): Prettify and add resume `<a>` link in `landing-hero-section.ts`

--- src/app/features/landing/landing-hero-section.ts
+++ src/app/features/landing/landing-hero-section.ts
@@ -1,33 +1,40 @@
-import { Component } from "@angular/core";
-import { environment } from "@environments/environment";
-import { ZardButtonComponent } from "@shared/components/button/button.component";
-import { ZardIconComponent } from "@shared/components/icon/icon.component";
+import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+import { ZardButtonComponent } from '@shared/components/button/button.component';
+import { ZardIconComponent } from '@shared/components/icon/icon.component';
 
 @Component({
   selector: 'landing-hero',
   standalone: true,
   imports: [ZardButtonComponent, ZardIconComponent],
   template: `
-  <section class="flex">
-    <div class="flex flex-col gap-lg max-w-line-length z-1">
-      <div>
-      <h1 class="text-hero-accent">Zooming</h1>
-      <h1 class="inline-block">Full Stack Developer</h1>
+    <section class="flex">
+      <div class="flex flex-col gap-lg max-w-line-length z-1">
+        <div>
+          <h1 class="text-hero-accent">Zooming</h1>
+          <h1 class="inline-block">Full Stack Developer</h1>
+        </div>
+        <p>
+          <span class="text-color-accent">{{ name }}</span> here — a software engineer experienced
+          in frontend and middleware development for ROS (Robot Operating System).
+        </p>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          [href]="'resumes/' + name.replace(' ', '_') + '_Resume.pdf'"
+          z-button
+          class="w-min"
+        >
+          <i z-icon zSize="lg" zType="resume"></i>
+          View Resume
+        </a>
       </div>
-      <p><span class="text-color-accent">{{ name }}</span> here — a software engineer experienced in frontend and middleware development for ROS (Robot Operating System).</p>
-      <button z-button class="w-min">
-        <i z-icon zSize="lg" zType="resume"></i>
-        View Resume
-      </button>
-    </div>
-    <video autoplay loop class="max-w-full">
-      <source src="assets/videos/motorcycle.mp4" type="video/mp4">
-    </video>
-  </section>
+      <video autoplay loop class="max-w-full">
+        <source src="assets/videos/motorcycle.mp4" type="video/mp4" />
+      </video>
+    </section>
   `,
-
-
 })
 export default class LandingHeroComponent {
-  protected readonly name = environment.name
+  protected readonly name = environment.name;
 }
