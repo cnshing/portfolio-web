14cfdd3cd1850e181d612104713cec1e73d380cf
Shing Chan <chan.shing@protonmail.com>
2026-01-14 22:11:06 -0800

    fix(button): Adjust accessibility ring color and sizing in `button.variants.ts`

--- src/app/shared/components/button/button.variants.ts
+++ src/app/shared/components/button/button.variants.ts
@@ -1,13 +1,13 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  "cursor-pointer inline-flex items-center justify-center min-h-fit gap-xs whitespace-nowrap rounded-xs text-lg font-medium transition-all active:scale-95 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive font-button",
+"cursor-pointer inline-flex items-center justify-center min-h-fit gap-xs whitespace-nowrap rounded-xs text-lg font-medium transition-all active:scale-95 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50   focus-visible:ring-[0.46875rem] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive font-button",
   {
     variants: {
       zType: {
-        default: 'bg-color-button text-on-bg-button shadow-xs hover:bg-color-button/default',
-        destructive: 'bg-destructive text-color-default shadow-xs hover:bg-destructive/strong focus-visible:ring-destructive/20',
-        outline: 'border-color-button border shadow-xs bg-color-page text-border-button hover:bg-color-surface1/default',
+        default: 'bg-color-button text-on-bg-button shadow-xs hover:bg-color-button/default focus-visible:ring-border-button/55',
+      destructive: 'bg-destructive text-color-default shadow-xs hover:bg-destructive/strong focus-visible:ring-destructive/28',
+        outline: 'border-color-button border shadow-xs bg-color-page text-border-button hover:bg-color-surface1/default focus-visible:ring-border-button/60',
         secondary: 'bg-color-surface2 border-color-subtle border text-secondary-foreground shadow-xs hover:bg-color-surface2/strong',
         ghost: 'text-on-bg-secondary bg-none hover:bg-color-surface2 hover:text-color-secondary',
         link: 'text-on-bg-default underline-offset-4 hover:underline',
