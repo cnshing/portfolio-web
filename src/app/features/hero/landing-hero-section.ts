96b9aa5f88c32f7a990b9ed47076bff79616df10
Shing Chan <chan.shing@protonmail.com>
2026-01-22 03:28:15 -0800

    refactor(landing)!: Move hero section to `hero` folder

--- src/app/features/hero/landing-hero-section.ts
+++ src/app/features/hero/landing-hero-section.ts
@@ -2,38 +2,38 @@ import { Component } from '@angular/core';
 import { environment } from '@environments/environment';
 import { ZardButtonComponent } from '@shared/components/button/button.component';
 import { ZardIconComponent } from '@shared/components/icon/icon.component';
-
+import { LandingHeroMotorcyclistComponent } from '@features/hero/landing-hero-motorcyclist';
 @Component({
   selector: 'landing-hero',
   standalone: true,
-  imports: [ZardButtonComponent, ZardIconComponent],
+  imports: [ZardButtonComponent, ZardIconComponent, LandingHeroMotorcyclistComponent],
   template: `
-    <section class="flex">
-      <div class="flex flex-col gap-lg max-w-line-length z-1">
+    <section class="relative flex">
+      <div class="flex flex-col gap-lg">
         <div>
-          <h1 class="text-hero-accent">Zooming</h1>
-          <h1 class="inline-block">Full Stack Developer</h1>
+          <h1 class="z-[1]">
+            <span class="text-hero-accent">Zooming</span>
+            <br />
+            Full Stack Developer
+          </h1>
         </div>
-        <p>
-          <span class="text-color-accent">{{ name }}</span> here — a software engineer experienced
-          in frontend and middleware development for ROS (Robot Operating System).
-        </p>
         <a
           target="_blank"
           rel="noopener noreferrer"
           [href]="'resumes/' + name.replace(' ', '_') + '_Resume.pdf'"
           z-button
-          class="w-min"
+          class="w-min z-[3]"
         >
           <i z-icon zSize="lg" zType="resume"></i>
           View Resume
         </a>
       </div>
-      <video disableRemotePlayback  autoplay loop class="max-w-full">
-        <source src="assets/videos/motorcycle.mp4" type="video/mp4" />
-      </video>
     </section>
+    <landing-hero-motorcyclist class="z-[2]" />
   `,
+  host: {
+    'class': 'relative z-0',
+  }
 })
 export default class LandingHeroComponent {
   protected readonly name = environment.name;
