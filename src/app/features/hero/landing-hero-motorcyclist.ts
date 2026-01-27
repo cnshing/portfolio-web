a075b85ae594896511501fb62255094d55d6efd6
Shing Chan <chan.shing@protonmail.com>
2026-01-26 21:27:35 -0800

    refactor(landing): Use relative postioning and adjust spacing in `landing-hero-motorcyclist.ts`

--- src/app/features/hero/landing-hero-motorcyclist.ts
+++ src/app/features/hero/landing-hero-motorcyclist.ts
@@ -7,8 +7,8 @@ import { VideoAutoplayDirective } from '@shared/directives/autoplay.directive';
   imports: [VideoAutoplayDirective],
   template: `
     <video
-      src="assets/videos/motorcycleman/test_motorcycle_idle.webm"
-      class="absolute bottom-[7.5vh] origin-bottom brightness-75 right-0 -mr-[57.5vw] md:mr-0 origin-bottom scale-y-[250%] -scale-x-[250%] translate-y-[2%] max-h-[calc((100vh-var(--spacing-2xl)+5%)/2.5)] overflow-x-hidden"
+      src="assets/videos/motorcycleman/test_motorcycle_idle_flipped.webm"
+      class="brightness-75 w-full ml-[50vw] origin-bottom scale-[250%] max-h-[min((100%-var(--spacing-2xl)+4.25%)/2.5,var(--spacing-2xl)*4)] overflow-x-hidden"
       disableRemotePlayback
       muted
       playsinline
@@ -18,12 +18,12 @@ import { VideoAutoplayDirective } from '@shared/directives/autoplay.directive';
     >
       <source
         type="video/quicktime; codecs=hvc1.1.6.H120.b0"
-        src="assets/videos/motorcycleman/test_motorcycle_idle.webm"
+        src="assets/videos/motorcycleman/test_motorcycle_idle_flipped.webm"
       />
     </video>
   `,
   host: {
-    class: 'absolute bottom-0 size-full',
+    class: 'absolute bottom-0 size-full flex flex-col justify-end pl-lg',
   },
   styles: [
     `
