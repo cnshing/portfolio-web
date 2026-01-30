93d48a2f34f7145c57798fc373f9b77da41be366
Shing Chan <chan.shing@protonmail.com>
2026-01-30 03:18:53 -0800

    feat(autoplay): Implement alternative autoplay directive in `autoplay.directive.ts`

--- src/app/shared/directives/autoplay.directive.ts
+++ src/app/shared/directives/autoplay.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { afterNextRender, DestroyRef, Directive, ElementRef, inject, signal } from "@angular/core";
 
 /**
  * Guranatees video will autoplay on any specified autoplay video. From https://stackoverflow.com/a/65290856 and https://stackoverflow.com/a/55940696.
@@ -19,3 +19,64 @@ import { Directive } from "@angular/core";
   },
 })
 export class VideoAutoplayDirective {}
+
+/**
+ *
+ * Autoplays video only when the element is visible. An alternative directive to force `autoplay` playback.
+ *
+ * @export
+ * @class AutoplayOnVisibleDirective
+ * @typedef {AutoplayOnVisibleDirective}
+ */
+@Directive({
+  selector: 'video[autoplayOnVisible]',
+  standalone: true,
+  host: {
+    '[attr.muted]': 'this.isVisible() ? "" : undefined',
+    '[attr.autoplay]': 'this.isVisible() ? "" : undefined',
+  },
+})
+export class AutoplayOnVisibleDirective {
+
+  private el = inject(ElementRef<HTMLVideoElement>);
+  private destroyRef = inject(DestroyRef);
+  private observer?: IntersectionObserver;
+
+
+  /**
+   * Is the video element visible?
+   *
+   * @readonly
+   * @type {boolean}
+   */
+  readonly isVisible = signal<boolean>(false);
+
+  constructor() {
+    afterNextRender(() => this.initObserver());
+  }
+
+  /**
+   * Configures autoplay when video element is observed
+   *
+   * @private
+   */
+  private initObserver(): void {
+    const video = this.el.nativeElement;
+    this.observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry?.isIntersecting) {
+          this.isVisible.set(true);
+          video.play();
+          this.observer?.disconnect();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    this.observer.observe(video);
+
+    this.destroyRef.onDestroy(() => {
+      this.observer?.disconnect();
+    });
+  }
+}
