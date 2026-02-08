471f8a3a81f655b39579869c121a7f7f08295696
Shing Chan <chan.shing@protonmail.com>
2026-02-08 00:44:33 -0800

    feat(core): Add image pixel density loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { IMAGE_LOADER_PROVIDER } from '@core/providers/image-loader.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    IMAGE_LOADER_PROVIDER
   ]
 };
