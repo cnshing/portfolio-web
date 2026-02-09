32369f6066f8ceac9d034320417abddaf9c50e97
Shing Chan <chan.shing@protonmail.com>
2026-02-09 05:51:43 -0800

    chore(landing): Add more comment detail for `landing-hero-motorcyclist.ts`

--- src/app/features/landing/hero/landing-hero-motorcyclist.ts
+++ src/app/features/landing/hero/landing-hero-motorcyclist.ts
@@ -174,7 +174,7 @@ const animateMotorcycle = (element: HTMLElement, enterDuration: number = 2.5): g
   standalone: true,
   imports: [VideoAutoplayDirective, TransparentVideoLinkComponent],
   template: `
-    <link selectTransparent rel="preload" as="video" zWebkitSrc="/assets/videos/motorcycle.mp4" zFallbackSrc="/assets/videos/motorcycle.webm" fetchpriority="high"/> <!-- TODO: The following code for rel=preload doesn't work. Figure out why <link rel=preload> has an invalid href value -->
+    <link selectTransparent rel="preload" as="video" zWebkitSrc="/assets/videos/motorcycle.mp4" zFallbackSrc="/assets/videos/motorcycle.webm" fetchpriority="high"/> <!-- TODO: The following code for rel=preload doesn't work, but rel=prefetch does. Figure out why <link rel=preload> has an invalid href value -->
     <video
       class="brightness-75 w-full ml-[50vw] origin-bottom scale-x-[-250%] scale-y-[250%] max-h-[min((100%-var(--spacing-2xl)+4.25%)/2.5,var(--spacing-2xl)*4)] overflow-x-hidden"
       [poster]="motorcyclePoster()"
