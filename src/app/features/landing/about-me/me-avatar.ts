9d8661faa1b3c5098b7ed9056814e54d2522c474
Shing Chan <chan.shing@protonmail.com>
2026-02-08 18:32:56 -0800

    feat(landing)!: Add WIP link preloading/prefetching for me-avatar and motorcycle assets

--- src/app/features/landing/about-me/me-avatar.ts
+++ src/app/features/landing/about-me/me-avatar.ts
@@ -1,6 +1,8 @@
-import { Component, computed, ElementRef, input, viewChild } from '@angular/core';
+import { Component, computed, ElementRef, inject, input, viewChild } from '@angular/core';
 import { ZardAvatarContainerComponent } from '@shared/components/avatar/avatar.component';
 import { AutoplayOnVisibleDirective, VideoAutoplayDirective } from '@shared/directives/autoplay.directive';
+import { SafeResourceUrlPipe } from '@shared/utils/sanitizers';
+import { Platform, PlatformModule } from '@angular/cdk/platform';
 
 export const postures = [
   'concentrating',
@@ -19,8 +21,16 @@ export type Postures = (typeof postures)[number];
 @Component({
   selector: 'me-avatar',
   standalone: true,
-  imports: [ZardAvatarContainerComponent, VideoAutoplayDirective, AutoplayOnVisibleDirective],
+  imports: [ZardAvatarContainerComponent, VideoAutoplayDirective, AutoplayOnVisibleDirective, SafeResourceUrlPipe, PlatformModule],
   template: `
+    @for (avatar of postures; track avatar) {
+      @if (isWebkit) {
+        <link rel="prefetch" [href]="avatarSrcPath(avatar) + '.mp4' | sanitizeResourceUrl"/>
+      }
+      @else {
+        <link rel="prefetch" [href]="avatarSrcPath(avatar) + '.webm' | sanitizeResourceUrl" />
+      }
+    }
     <z-avatar-container
       class="rounded-full aspect-square overflow-hidden"
       zSize="none"
@@ -43,10 +53,23 @@ export class LandingAboutMeAvatarComponent {
   readonly posture = input.required<Postures>();
   protected readonly video = viewChild.required<ElementRef>('avatarVideo')
   readonly avatarSrc = computed(() => {
-    const src = `/assets/avatars/me-${this.posture()}`
+    const src = this.avatarSrcPath(this.posture())
     const videoElement: HTMLVideoElement = this.video().nativeElement
     videoElement.load() // Dynamic binding in Angularv20 to <source>'s src tag fails: https://stackoverflow.com/questions/39180415/angular-2-change-videos-src-after-clicking-on-div#comment112049463_39237248
     videoElement.playbackRate = 0.91
     return src
   });
+
+
+  /**
+   *
+   *
+   * @param {Postures} posture
+   * @returns {string} Avatar source path of `posture`
+   */
+  protected readonly avatarSrcPath = (posture: Postures) => `/assets/avatars/me-${posture}`
+
+  protected readonly postures = postures
+
+  protected readonly isWebkit = inject(Platform).WEBKIT
 }
