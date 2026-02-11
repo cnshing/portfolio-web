82ecf973cf467f6fff96b4657151192fa34a1b0c
Shing Chan <chan.shing@protonmail.com>
2026-02-11 07:19:44 -0800

    revert(app): Remove un-needed client hydration in `app.config.ts`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { IMAGE_LOADER_PROVIDER } from '@core/providers/image-loader.provider';
 
@@ -9,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
     provideHttpClient(withFetch()),
     IMAGE_LOADER_PROVIDER
   ]
