f4141605d1bd5c1f6a2bacd881a221fe2a845131
Shing Chan <chan.shing@protonmail.com>
2026-02-18 00:51:18 -0800

    feat(landing): Add animated github icon `landing-about-me-github.ts`

---
+++ src/app/features/landing/about-me/landing-about-me-github.ts
@@ -0,0 +1,37 @@
+import { Component, viewChild } from '@angular/core';
+import { ZardIconComponent } from '@shared/components/icon/icon.component';
+import {
+  DeferredLottieComponent,
+  OptimizedLottieComponent,
+} from '@shared/components/lottie/lottie.component';
+
+/**
+ * Animated Github Icon
+ *
+ * @export
+ * @class LandingAboutMeGithubComponent
+ * @typedef {LandingAboutMeGithubComponent}
+ */
+@Component({
+  selector: 'landing-about-me-github-icon',
+  standalone: true,
+  imports: [ZardIconComponent, DeferredLottieComponent, OptimizedLottieComponent],
+  template: `
+    <deferred-lottie
+      class="inline-flex align-top size-(--text-xl)"
+      [placeholder]="poster"
+      [content]="animation"
+    >
+    </deferred-lottie>
+
+    <ng-template #poster>
+      <i z-icon zType="githubICO" zSize="lg"> </i>
+    </ng-template>
+
+    <ng-template #animation>
+      <optimized-lottie #github src="/assets/icons/github.lottie" loop [speed]="3.0" />
+    </ng-template>
+  `,
+})
+export default class LandingAboutMeGithubComponent {
+}
