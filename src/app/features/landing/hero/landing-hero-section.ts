a064f9913715599aa727991cb3711d81d77edb57
Shing Chan <chan.shing@protonmail.com>
2026-02-24 23:38:22 -0800

    refactor(landing): Add DOM raindrop effect to landing page

--- src/app/features/landing/hero/landing-hero-section.ts
+++ src/app/features/landing/hero/landing-hero-section.ts
@@ -4,6 +4,8 @@ import { ZardButtonComponent } from '@shared/components/button/button.component'
 import { ZardIconComponent } from '@shared/components/icon/icon.component';
 import { LandingHeroMotorcyclistComponent } from '@features/landing/hero/landing-hero-motorcyclist';
 import { LandingHeroAffordanceComponent } from '@features/landing/hero/landing-hero-affordance';
+import { LandingHeroRainComponent } from '@features/landing/hero/landing-hero-rain';
+
 @Component({
   selector: 'landing-hero',
   standalone: true,
@@ -12,12 +14,13 @@ import { LandingHeroAffordanceComponent } from '@features/landing/hero/landing-h
     ZardIconComponent,
     LandingHeroMotorcyclistComponent,
     LandingHeroAffordanceComponent,
+    LandingHeroRainComponent
   ],
   template: `
     <section class="relative flex">
       <div class="flex flex-col gap-lg">
         <div>
-          <h1 class="relative text-hero-accent z-[4]">Zooming</h1>
+          <h1 class="relative text-hero-accent z-[3]">Zooming</h1>
           <h1 class="relative z-[1]">Full Stack Developer</h1>
         </div>
         <a
@@ -25,7 +28,7 @@ import { LandingHeroAffordanceComponent } from '@features/landing/hero/landing-h
           rel="noopener noreferrer"
           [href]="'resumes/' + name.replace(' ', '_') + '_Resume.pdf'"
           z-button
-          class="w-min z-[3] group"
+          class="w-min z-[5] group"
         >
           <i
             class="-rotate-9 group-hover:-rotate-19 group-focus:-rotate-19 transition-transform duration-250"
@@ -39,10 +42,13 @@ import { LandingHeroAffordanceComponent } from '@features/landing/hero/landing-h
     </section>
     <landing-hero-motorcyclist class="z-[2]" />
     <landing-hero-affordance
-      class="absolute bottom-0 translate-y-[calc(var(--racetrack-height)/2.5)] -translate-x-1/2 left-1/2 z-[4] text-color-accent brightness-75 animate-bounce *:data-motorcycle:text-[9vw] *:data-arrow:text-[7.5vw] *:md:data-motorcycle:text-2xl *:md:data-arrow:text-xl
+      class="absolute bottom-0 translate-y-[calc(var(--racetrack-height)/2.5)] -translate-x-1/2 left-1/2 z-[6] text-color-accent brightness-75 animate-bounce *:data-motorcycle:text-[9vw] *:data-arrow:text-[7.5vw] *:md:data-motorcycle:text-2xl *:md:data-arrow:text-xl
       [animation-duration:2500ms]transition-opacity duration-1500 ease-in-out"
       [idleTimeoutMS]="3250"
     />
+    @defer (on idle) {
+    <landing-hero-rain class="absolute top-0 -z-[4] w-full h-[calc(100%+var(--racetrack-height)+var(--spacing-3xs))] overflow-hidden"/>
+    }
   `,
   host: {
     class: 'relative z-0',
@@ -51,3 +57,5 @@ import { LandingHeroAffordanceComponent } from '@features/landing/hero/landing-h
 export default class LandingHeroComponent {
   protected readonly name = environment.name;
 }
+
+// h-[calc(100%+var(--racetrack-height))]
