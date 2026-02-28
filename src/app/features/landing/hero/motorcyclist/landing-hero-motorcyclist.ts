dcb62229a8ad5056521c2bcf546c48aa1d9d323b
Shing Chan <chan.shing@protonmail.com>
2026-02-28 06:48:51 -0800

    chore(landing): Prettify `landing-hero-motorcyclist.ts`

--- src/app/features/landing/hero/motorcyclist/landing-hero-motorcyclist.ts
+++ src/app/features/landing/hero/motorcyclist/landing-hero-motorcyclist.ts
@@ -1,5 +1,15 @@
-import { afterNextRender, Component, computed, DestroyRef, ElementRef, inject, input, signal, viewChild } from "@angular/core";
-import { VideoAutoplayDirective } from "@shared/directives/autoplay.directive";
+import {
+  afterNextRender,
+  Component,
+  computed,
+  DestroyRef,
+  ElementRef,
+  inject,
+  input,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { VideoAutoplayDirective } from '@shared/directives/autoplay.directive';
 
 /**
  * Motorcyclist element with built-in animations.
@@ -21,22 +31,22 @@ import { VideoAutoplayDirective } from "@shared/directives/autoplay.directive";
       playsinline
       loop
       fetchpriority="high"
-      preload='metadata'
+      preload="metadata"
       autoplay
       #heroMotorcyclist
       [class.animate-(--animate-motorcyclist-enter)]="!animationModuleReady()"
-      [style.animation-composition]="animationModuleReady()? null : 'add'"
+      [style.animation-composition]="animationModuleReady() ? null : 'add'"
     >
       <source type="video/quicktime; codecs=hvc1.1.6.H120.b0" [src]="motorcycleSrc() + '.mp4'" />
       <source type="video/webm; codecs=vp09.00.41.08" [src]="motorcycleSrc() + '.webm'" />
     </video>
   `,
   host: {
-    '[style.--enter-duration]': "enterDurationSecs()", // Value must be unitless due to CSS animation implementation
+    '[style.--enter-duration]': 'enterDurationSecs()', // Value must be unitless due to CSS animation implementation
     class:
       'absolute bottom-0 size-full flex flex-col justify-end pl-lg overflow-x-hidden overflow-y-hidden',
   },
-  styleUrl: 'landing-hero-motorcyclist-animation.sass'
+  styleUrl: 'landing-hero-motorcyclist-animation.sass',
 })
 export class LandingHeroMotorcyclistComponent {
   /**
@@ -49,12 +59,16 @@ export class LandingHeroMotorcyclistComponent {
       const vehcile = this.video().nativeElement;
       vehcile.load(); // TODO: When possible make a directive to fix Angular video loading with multiple sources
       vehcile.play();
-      vehcile.addEventListener('animationend', async (_: AnimationEvent) => { // NOTE: Event data in Safari only has `isTrusted` property
-        const { animateMotorcycle } = await import('./landing-hero-motorcyclist-animation');
-        this.animationModuleReady.set(true);
-        this.animate.set(animateMotorcycle(vehcile, this.enterDurationSecs()));
-
-      }, { once: true });
+      vehcile.addEventListener(
+        'animationend',
+        async (_: AnimationEvent) => {
+          // NOTE: Event data in Safari only has `isTrusted` property
+          const { animateMotorcycle } = await import('./landing-hero-motorcyclist-animation');
+          this.animationModuleReady.set(true);
+          this.animate.set(animateMotorcycle(vehcile, this.enterDurationSecs()));
+        },
+        { once: true }
+      );
 
       // Start loading the animation module (will be cached for later use)
       await import('./landing-hero-motorcyclist-animation');
@@ -72,15 +86,13 @@ export class LandingHeroMotorcyclistComponent {
    */
   protected readonly animate = signal<any>(null);
 
-
   /**
    * How long does it take for the motorcycle to enter in seconds?
    *
    * @readonly
    * @type {*}
    */
-  readonly enterDurationSecs = input<number>(2.0)
-
+  readonly enterDurationSecs = input<number>(2.0);
 
   /**
    * Is the lazy-loaded animation module ready?
@@ -89,7 +101,7 @@ export class LandingHeroMotorcyclistComponent {
    * @readonly
    * @type {*}
    */
-  protected readonly animationModuleReady = signal<boolean>(false)
+  protected readonly animationModuleReady = signal<boolean>(false);
 
   /**
    * Motorcycle element to animate.
