6a8fe04a0d183fbcb2e29ff0a88830f3d8ee19a2
Shing Chan <chan.shing@protonmail.com>
2026-03-11 21:23:13 -0700

    feat(landing): Add enter fade animation to landing hero starfield

--- src/app/features/landing/hero/starfield/landing-hero-starfield-shaders.ts
+++ src/app/features/landing/hero/starfield/landing-hero-starfield-shaders.ts
@@ -1,5 +1,5 @@
 /**
- * GLSL shader equivalent of https://www.youtube.com/watch?v=XUtIzRkaQOE
+ * GLSL shader equivalent of https://www.youtube.com/watch?v=XUtIzRkaQOE. Coupled with `vertexShader()`.
  *
  * @param {number} [fadeBias=0.1]
  * Controls how quickly the particle fades from the center toward the edge.
@@ -14,14 +14,39 @@
  * @returns {string} GLSL fragment shader source.
  */
 export const fragmentShader = (fadeBias: number= 0.1, glowStrength: number = 0.2) => /* glsl */ `
-
+varying float vReveal;
 void main() {
 
     float d = max(length(gl_PointCoord - 0.5), 0.0001);
     float alpha = clamp(${fadeBias} / d - ${glowStrength}, 0.0, 1.0);
+    alpha *= vReveal;
     csm_DiffuseColor = vec4(vColor, alpha);
 
 }
 `;
 
 
+
+/**
+ * Vertex shader that handles initial appear animation of the starfield.
+ *
+ * @returns {string}
+ */
+export const vertexShader = () => /* language:glsl glsl */ `
+uniform float time;
+varying float vReveal;
+
+void main() {
+
+    float dist = length(position - cameraPosition);
+
+    float radius = time * 20.0;
+
+    float reveal = 1.0 - smoothstep(radius - 50.0, radius, dist);
+
+    vReveal = reveal;
+
+    float baseSize = csm_PointSize;
+    csm_PointSize = baseSize * vReveal;
+}
+`
